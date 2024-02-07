490f11189ddb93e25cac47151297ce50212c427f
etachott <bfwaend@gmail.com>
2024-02-07 20:22:32 -0300

    Update tests on main

--- data_structures/linked_list/main.c
+++ data_structures/linked_list/main.c
@@ -2,6 +2,9 @@
 
 int main() {
 	singly_linked_list *list = new_singly_linked_list();
+	node *head = create_node(42);
+	list->head = head;
+	print_singly_linked_list(list);
 	delete_singly_linked_list(&list);
 	return 0;
 }
