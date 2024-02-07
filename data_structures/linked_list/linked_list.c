c4d4274c0051e0f8380d5b75a55b3ca57cbf34fa
etachott <bfwaend@gmail.com>
2024-02-07 20:22:23 -0300

    Add simple singly linked list operations

--- data_structures/linked_list/linked_list.c
+++ data_structures/linked_list/linked_list.c
@@ -1,4 +1,6 @@
 #include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 singly_linked_list *new_singly_linked_list() {
 	singly_linked_list *new = calloc(1, sizeof(singly_linked_list));
@@ -8,7 +10,7 @@ singly_linked_list *new_singly_linked_list() {
 void delete_singly_linked_list(singly_linked_list **list) {
 	node *prev_node = (*list)->head;
 
-	while (*list && prev_node) {
+	while ((*list)->head && prev_node) {
 		prev_node = (*list)->head;
 		(*list)->head = (*list)->head->next;
 		prev_node->data = 0;
@@ -18,4 +20,37 @@ void delete_singly_linked_list(singly_linked_list **list) {
 	free(*list);
 	list = NULL;
 	return ;
+}
+
+void print_node(node *n) {
+	printf("Data = %d | Next = %p\n", n->data, n->next);
+}
+
+void print_singly_linked_list(singly_linked_list *list) {
+	int i = 0;
+	for (node *n = list->head; n != NULL; n = n->next) {
+		printf("Node %d: ", i);
+		print_node(n);
+		i++;
+	}
+}
+
+node *get_last_singly_linked_list(singly_linked_list *list) {
+	node *n = list->head;
+	for (; n->next != NULL; n = n->next) {}
+	return n;
+}
+
+void add_node_to_sl_list(node *new_node, singly_linked_list **list) {
+	node *n = get_last_singly_linked_list(*list);
+	n->next = new_node;
+	return ;
+}
+
+node *create_node(int data) {
+	node *new_node = calloc(1, sizeof(node));
+
+	new_node->data = data;
+	new_node->next = NULL;
+	return new_node;
 }
--- data_structures/linked_list/linked_list.h
+++ data_structures/linked_list/linked_list.h
@@ -15,4 +15,9 @@ typedef struct singly_linked_list {
 
 singly_linked_list	*new_singly_linked_list(void);
 void				delete_singly_linked_list(singly_linked_list **list);
+void				print_node(node *n);
+void				print_singly_linked_list(singly_linked_list *list);
+node				*get_last_singly_linked_list(singly_linked_list *list);
+void				add_node_to_sl_list(node *new_node, singly_linked_list **list);
+node				*create_node(int data);
 #endif // !LINKED_LIST_H
